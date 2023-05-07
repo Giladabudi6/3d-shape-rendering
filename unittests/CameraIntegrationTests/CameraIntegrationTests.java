22119ab0226b46114f1fb1856557109bf3013fe1
Giladabudu6 <giladabudi6@gmail.com>
2023-05-07 16:36:24 +0300

    finish integrationTests of sphere,plane,triangle

--- unittests/CameraIntegrationTests/CameraIntegrationTests.java
+++ unittests/CameraIntegrationTests/CameraIntegrationTests.java
@@ -1,7 +1,9 @@
 package CameraIntegrationTests;
 
 import geometries.Intersectable;
+import geometries.Plane;
 import geometries.Sphere;
+import geometries.Triangle;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Vector;
@@ -75,29 +77,35 @@ public class CameraIntegrationTests {
         camera1.setVPSize(3,3);
         camera1.setVPDistance(1);
 
-        Camera camera2 = new Camera(zeroPoint, new Vector(0, 0, -0.5), new Vector(0, -1, 0));
-        camera2.setVPSize(3,3);
-        camera2.setVPDistance(1);
 
+        //TC06: Plane normal=(0,-1,0) (9 intersections)
+        assertEquals(9,countIntersectionsCameraGeometry(camera1,3,3,new Plane(new Point(0,-2,0),new Vector(0,-1,0))),"Wrong number of intersections");
 
-        //TC01: Sphere r=1 (2 intersections)
-        assertEquals(2,countIntersectionsCameraGeometry(camera1,3,3,new Sphere(1,new Point(0,0,-3))),"Wrong number of intersections");
+        //TC07: Plane normal=(0,-1,0.5) (9 intersections)
+        assertEquals(9,countIntersectionsCameraGeometry(camera1,3,3,new Plane(new Point(0,-2,0),new Vector(0,-1,0.5))),"Wrong number of intersections");
 
-        //TC02: Sphere r=2.5 (18 intersections)
-        assertEquals(18,countIntersectionsCameraGeometry(camera2,3,3,new Sphere(2.5,new Point(0,0,-2.5))),"Wrong number of intersections");
-
-        //TC03: Sphere r=2 (10 intersections)
-        assertEquals(10,countIntersectionsCameraGeometry(camera2,3,3,new Sphere(2,new Point(0,0,-2))),"Wrong number of intersections");
-
-        //TC04: Sphere r=4 (9 intersections)
-        //TODO: find the correct point
-        assertEquals(9,countIntersectionsCameraGeometry(camera2,3,3,new Sphere(4,new Point(0,0,0))),"Wrong number of intersections");
-
-        //TC05: Sphere r=0.5 (0 intersections)
-        assertEquals(0,countIntersectionsCameraGeometry(camera1,3,3,new Sphere(0.5,new Point(0,0,1))),"Wrong number of intersections");
+        //TC08: Plane normal=(0,-1,1) (6 intersections)
+        assertEquals(6,countIntersectionsCameraGeometry(camera1,3,3,new Plane(new Point(0,-2,0),new Vector(0,-1,0.5))),"Wrong number of intersections");
 
     }
 
+
+    @Test
+        // Test methods for integration between Camera and Triangle
+    void testCameraTriangleIntegration() {
+
+        Camera camera1 = new Camera(zeroPoint, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        camera1.setVPSize(3,3);
+        camera1.setVPDistance(1);
+
+
+        //TC09: Triangle (1 intersections)
+        assertEquals(1,countIntersectionsCameraGeometry(camera1,3,3,new Triangle(new Point(0,1,-2),new Point(1,-1,-2),new Point(-1,-1,-2))),"Wrong number of intersections");
+
+        //TC10: Triangle (2 intersections)
+        assertEquals(2,countIntersectionsCameraGeometry(camera1,3,3,new Triangle(new Point(0,20,-2),new Point(1,-1,-2),new Point(-1,-1,-2))),"Wrong number of intersections");
+
+    }
 
 
 }
